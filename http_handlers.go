249ec00cce774450e310e9c578cd3ab5787e6be2
Yui Sakamoto <y-sakamoto@tyrellsys.com>
2014-09-08 00:55:35 +0900

    Add http_handlers.

---
+++ http_handlers.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+  "fmt"
+  "net/http"
+)
+
+type String string
+
+type Struct struct {
+  Greeting string
+  Punct    string
+  Who      string
+}
+
+func (h String) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+  fmt.Fprint(w, h)
+}
+
+func (h Struct) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+  fmt.Fprint(w, h.Greeting, h.Punct, h.Who)
+}
+
+func main() {
+  http.Handle("/string", String("I'm yui_tang"))
+  http.Handle("/struct", &Struct{"Hello", ":", "Gophers!"})
+  http.ListenAndServe("localhost:4000", nil)
+}
+
+// Access to http://localhost:4000/(string|struct)
